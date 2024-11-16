1a834d302fd10843698b933612d3febc4ce6a948
corythchambers <corythchambers@gmail.com>
2024-11-16 09:14:40 -0800

    Adding expiration logic to local storage

--- javascript_jquery/exercises/ch14/task_list/task_list.js
+++ javascript_jquery/exercises/ch14/task_list/task_list.js
@@ -9,6 +9,12 @@ $(document).ready( () => {
             alert("Please enter a task.");
             textbox.focus();
         } else {
+            // get expiration date
+            const expirationDate = new Date();
+            expirationDate.setDate(expirationDate.getDate() + 21)
+
+            localStorage.expiration = expirationDate;
+
             // add task to web storage 
             let tasks = localStorage.E14tasks || "";  // "" is default
             localStorage.E14tasks = tasks.concat(task, "\n");
@@ -23,11 +29,22 @@ $(document).ready( () => {
     
     $("#clear_tasks").click( () => {
         localStorage.removeItem("E14tasks");
+        localStorage.removeItem("expiration")
         $("#task_list").val("");
         $("#task").focus();
     }); 
     
     // display tasks on initial load
-    $("#task_list").val(localStorage.E14tasks);
+    let storedExpiration = localStorage.expiration;
+    storedExpiration = Date.parse(storedExpiration);
+    storedExpiration = new Date(storedExpiration);
+    let todaysDate = new Date();
+
+    if (storedExpiration.getTime() < todaysDate.getTime()) {
+        localStorage.removeItem("E14tasks");
+        localStorage.removeItem("expiration")
+    } else {
+        $("#task_list").val(localStorage.E14tasks);
+    }
     $("#task").focus();
 });
